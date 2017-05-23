a5708f2ed205b94a3c51dfb878c6c1fa0198b196
Heejong Park <hpar081@aucklanduni.ac.nz>
2017-05-23 15:17:46 +1200

    update: adding bootstrap modal

--- server/public/javascript.js
+++ server/public/javascript.js
@@ -1,20 +1,9 @@
-$(function(){
-$('#dropdown-click-hz').click(function (){
-	$('.dropdown-toggle').children().first().text('Hz  ')
-})
-$('#dropdown-click-khz').click(function (){
-	$('.dropdown-toggle').children().first().text('Khz ')
-})
-$('#wave').change(function(e){
-	var fr = new FileReader()
-	fr.onload = function (e) {
-		const arr = e.target.result.split("\n")
-		var ctx = $('#input-chart')
+function createDatasets(data) {
 		var data = {
-				labels: Array.apply(null, {length: arr.length}).map(Number.call, Number),
+				labels: data['labels'],
 				datasets: [
 					{
-						label: "Input wave samples",
+						label: data['datalabel'],
 						fill: false,
 						lineTension: 0.1,
 						backgroundColor: "rgba(75,192,192,0.4)",
@@ -32,15 +21,43 @@ $('#wave').change(function(e){
 						pointHoverBorderWidth: 2,
 						pointRadius: 1,
 						pointHitRadius: 10,
-						data: arr,
-//             data: [65, 59, 80, 81, 56, 55, 40],
+						data: data['datasets'],
 						spanGaps: false
 					}
 		    ]
 		}
+	return data;
+}
+
+$(function(){
+$('#dropdown-click-hz').click(function (){
+	$('.dropdown-toggle').children().first().text('Hz  ')
+})
+$('#dropdown-click-khz').click(function (){
+	$('.dropdown-toggle').children().first().text('Khz ')
+})
+$('#wave').change(function(e){
+	var fr = new FileReader()
+	fr.onload = function (e) {
+		const arr = e.target.result.split("\n")
+		var ctx = $('#input-chart')
+		var data = createDatasets({
+			'labels': Array.apply(null, {length: arr.length}).map(Number.call, Number),
+			'datasets': arr,
+			'datalabel': 'Wave input'
+		})
 		var chart = new Chart(ctx, {
 			type: 'line',
 			data: data,
+				options: {
+					scales: {
+						xAxes: [{
+							ticks: {
+								maxTicksLimit: 30
+							}
+						}]
+					}
+				}
 		})
 	}
 	fr.readAsText(e.target.files[0])
@@ -48,6 +65,17 @@ $('#wave').change(function(e){
 $('form[id=main-submit]').on('submit', function(e) {
 	e.preventDefault()
 	var data = new FormData(this)
+	if(typeof(data.get('wave')) != 'object' || data.get('freq').trim() == '') {
+		if(!$('.alert').length){
+			$('#alert').prepend("<div class='alert alert-warning alert-dismissible' role='alert'> \
+				<button type='button' class='close' data-dismiss='alert' aria-label='Close'>\
+				<span aria-hidden='true'>&times;</span></button>\
+				<strong>Warning!</strong> Missing required input</div>")
+		}
+		$('#my-modal').modal('hide')
+		return;
+	}
+
 	data.append('unit', $('.dropdown-toggle').children().first().text().trim())
 	$.ajax({
 		url: "/submit",
@@ -56,7 +84,51 @@ $('form[id=main-submit]').on('submit', function(e) {
 		processData: false,
 		contentType: false,
 		success: function(data, status) { 
-			console.log(data)
+			var sample_count = data.sample_count
+			var ctx = $('#freq-chart')
+			var dataz = createDatasets({
+				'labels': Array.apply(null, {length: sample_count.length}).map(Number.call, Number),
+				'datasets': sample_count,
+				'datalabel': 'Frequency'
+			})
+			var chart = new Chart(ctx, {
+				type: 'line',
+				data: dataz,
+				options: {
+					scales: {
+						yAxes: [{
+							ticks: {
+								min: 0,
+							}
+						}],
+						xAxes: [{ ticks: {maxTicksLimit: 20 }}]
+					}
+				}
+			})
+			var sym_val = data.sym_val
+			ctx = $('#sym-chart')
+			dataz = createDatasets({
+				'labels': Array.apply(null, {length: sym_val.length}).map(Number.call, Number),
+				'datasets': sym_val,
+				'datalabel': 'Correlation result'
+			})
+			chart = new Chart(ctx, {
+				type: 'line',
+				data: dataz,
+				options: {
+					scales: {
+						xAxes: [{
+							ticks: {
+								min: 0,
+								maxTicksLimit: 20
+							}
+						}]
+					}
+				}
+			})
+			$('#my-modal').modal('hide')
+			if($('.alert').length)
+				$('.alert').alert('close')
 		},
 		error: function(err) {
 			console.log(err.statusText)
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const rs = require('randomstring')
 const os = require('os')
 const spawn = require('child_process').spawn
 const path = require('path')
+const readline = require('readline')
 
 const SERVER_PORT = 8080
 var xmlString
@@ -19,10 +20,13 @@ app.use('/jquery', express.static(__dirname + '/node_modules/jquery'));
 app.use('/chart.js', express.static(__dirname + '/node_modules/chart.js'));
 
 app.post('/submit', upload.single('wave'), function(req,res){
+	var freq = parseInt(req.body.freq)
+	var unit = req.body.unit
+	if(unit == "Khz")
+		freq = freq * 1000
 	var input = rs.generate(7)
 	var waveFile = (os.tmpdir() + "/" + input + '.txt').replace(/\\/g, '/')
 	var xmlFile = (os.tmpdir() + "/" + input + '.xml').replace(/\\/g, '/')
-	console.log(waveFile)
 	var xmlContent = xmlString.replace("$WAVE", waveFile)
 	var p1 = new Promise(function (resolve,reject) {
 		fs.writeFile(waveFile, req.file.buffer.toString(), function(err){
@@ -46,18 +50,25 @@ app.post('/submit', upload.single('wave'), function(req,res){
 			console.log('starting thread')
 			const ls = spawn('bash', ['-c', `CLASSPATH=../bin ../systemj/bin/sysjr ${xmlFile}`])
 			var sample_count = []
-			ls.stdout.on('data', (data) => {
+			var sym_val = []
+			var rl = readline.createInterface({
+				input: ls.stdout
+			})
+			rl.on('line', (data) => {
 				var o = JSON.parse(data.toString())
 				if(o.sample_count != null)
-					sample_count.push(o.sample_count)	
+					sample_count.push(freq / o.sample_count)	
+				if(o.sym_val != null)
+					sym_val.push(o.sym_val)
 			})
 			ls.stderr.on('data', (data) => {console.log(data.toString())})
 			ls.on('error', (data) => {reject('error on creating a thread')})
-			ls.on('exit', (status) => {resolve(sample_count)})
+			ls.on('exit', (status) => {resolve({'sample_count': sample_count, 'sym_val': sym_val})})
 		}
-	)}).then((sample_count) => {
-		res.status(200).json({"sample_count": sample_count })
+	)}).then((data) => {
+		res.status(200).json(data)
 	}).catch((d) => {
+		console.log(d)
 		res.status(400).send(d)
 	})
 })
